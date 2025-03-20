fc95317968e4b668e0928b637454687b389ce83f
gurtlssk <gurtlssk@gmail.com>
2025-03-20 10:30:44 +0900

    scheduler test

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import express from 'express';
 import 'dotenv/config'
-
+import 'node-cron';
+import { schedule } from 'node-cron';
+import { crawling } from './scheduler.js';
 
 const app = express();
 
 const port = process.env.PORT;
 
-const myLogger = (req,res,next) => {
-    console.log('t');
-};
-
-const time = (req,res,enxt) => {
+const time = (req,res,next) => {
     console.log('time:', new Date());
 }
 
-
-// app.use(myLogger);
+const task = schedule('30 * * * * *',() => {
+    crawling();
+},{
+    scheduled: false
+});
 
 app.use(time);
 
 app.get('/',(req,res) => { res.sendStatus('')});
 
-app.listen(port, () => {console.log(`${port}`)});
+app.listen(port, () => {
+    console.log(`${port}`);
+    task.start();
+});
 
 app.get('/p',(req,res) => {
     res.status(200).send('ok');
 });
-
-app.route('/').get().post().patch().delete();
---
+++ src/scheduler.js
@@ -0,0 +1,5 @@
+const crawling = () =>{
+    console.log('time:', new Date());
+}
+
+export { crawling };
